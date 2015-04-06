77bcb6a87cd9122d359f8b7ca824548d8b93ae9f
ben mccary <benmccary@benmccary-MacBookPro>
2015-04-06 00:41:45 -0700

    added overlay

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,6 +1,26 @@
-$
-( document ).ready(function(){
-
-//
-
-});
+$( document ).ready(function(){
+    $('.btn-close').fadeOut(200);
+    var open = false;
+    $('.btn-open').on('click', function(){
+        if (open == false) {
+            $('.overlay, .btn-close').fadeIn(200);
+            $('nav a, .btn-open').fadeOut(200);
+            open = true; 
+    } else {
+        $('.overlay, .btn-close').fadeOut(200);
+        $('nav a, .btn-close').fadeIn(200);
+        open = false;
+    }
+    }); 
+    
+    
+    $('.overlay, .btn-close').on('click', function(){
+        $('.overlay, .btn-close').fadeOut(200);
+        $('nav a, .btn-open').fadeIn(200);
+        open = false; 
+    });
+    
+    $('.wrap').on('click', function(){
+        return false; 
+    });
+})
