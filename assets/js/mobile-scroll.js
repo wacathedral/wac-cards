22da1f66151d8ffdd8b70e13dd94c794fe5a7cb0
ben mccary <benmccary@benmccary-MacBookPro>
2015-04-07 16:30:53 -0700

    trying to make animate scrollLeft work. Want to kill myself

--- assets/js/mobile-scroll.js
+++ assets/js/mobile-scroll.js
@@ -1,26 +1,4 @@
 $( document ).ready(function(){
-    $('.btn-close').fadeOut(200);
-    var open = false;
-    $('.btn-open').on('click', function(){
-        if (open == false) {
-            $('.overlay, .btn-close').fadeIn(200);
-            $('nav a, .btn-open').fadeOut(200);
-            open = true; 
-    } else {
-        $('.overlay, .btn-close').fadeOut(200);
-        $('nav a, .btn-close').fadeIn(200);
-        open = false;
-    }
-    }); 
-    
-    
-    $('.overlay, .btn-close').on('click', function(){
-        $('.overlay, .btn-close').fadeOut(200);
-        $('nav a, .btn-open').fadeIn(200);
-        open = false; 
-    });
-    
-    
 
 var tempScrollLeft = 0;
 var currentScrollLeft = 0;
@@ -57,6 +35,8 @@ $(window).off('scroll', scrollIt);
     }
 
 
-}    
-    
+}
+
+$(window).on('scroll', scrollIt);
 });
+
