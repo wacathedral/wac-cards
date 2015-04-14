1d2780545957475670788f8ae7b6b28d89c97c6f
ben mccary <benmccary@benmccary-MacBookPro>
2015-04-13 18:16:45 -0700

    tried slick carousel. Doesn't work

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -21,6 +21,9 @@ $( document ).ready(function(){
     });
     
     
+
     
     
 });
+
+
--- _site/assets/js/mobile-scroll.js
+++ _site/assets/js/mobile-scroll.js
@@ -1,42 +1,14 @@
-$( document ).ready(function(){
+$(document).ready(function() {
+  var scrollWidth = $(window).width();
 
-var tempScrollLeft = 0;
-var currentScrollLeft = 0;
-var scrollWidth = $(window).width();
-var newWidth = 0;
-
-
-function scrollIt() {
-
-$(window).off('scroll', scrollIt);
-
-    currentScrollLeft = $(window).scrollLeft();
-
-
-    if (tempScrollLeft < currentScrollLeft) {
-       newWidth = newWidth + scrollWidth;
-       $('.container').animate({scrollLeft: newWidth}, 500, function(){
-             var setScroll = setTimeout(function(){
-                console.log('Animation Complete');
-                tempScrollLeft = $(window).scrollLeft();
-                $(window).on('scroll', scrollIt);
-            }, 10);
-        }); 
-
-    } else if (tempScrollLeft > currentScrollLeft){
-       newWidth = newWidth - scrollWidth;
-       $('.container').animate({scrollLeft: newWidth}, 500, function(){
-             var setScroll = setTimeout(function(){
-                console.log('Animation Complete');
-                tempScrollLeft = $(window).scrollLeft();
-                $(window).on('scroll', scrollIt);
-            }, 10);
-        }); 
-    }
-
-
-}
-
-$(window).on('scroll', scrollIt);
+$('#card-container').on( 'scroll' function(){
+//add if statement
+    if($(this).scrollLeft()>= 10){
+    //add width scroll function
+        $('#card-container').animate({scrollLeft:"+="+scrollWidth}, 'slow');
+        return false;
+      }
+    });
 });
+
 
